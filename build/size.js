033b4269be43d96dc22cabc0627ace020e693a30
Krzysztof Jamroz <jamro@jamro.net>
2019-01-26 09:52:54 +0100

    build: improve size stats after build

--- build/size.js
+++ build/size.js
@@ -1,15 +1,42 @@
 #!/usr/bin/env node
 'use strict'
 
-const getSize = require('get-folder-size');
+const fs = require('fs');
 
-getSize('./dist', /gwgc201819_overlay\.png/, (err, size) => {
-  if (err) { throw err; }
-  let limit = 128000;
-  let percentage = 100*(size/limit)
-  console.log(`Size: ${size} bytes (${percentage.toFixed(1)}%)`);
-  if(size > limit ) {
-    console.error('The file exceeded the limit');
-    process.exit(1);
-  }
-});
+function rightPad(txt, len, char) {
+  char = char || " ";
+  while (txt.length < len) txt += char;
+  return txt;
+}
+
+function leftPad(txt, len, char) {
+  txt = txt + "";
+  char = char || " ";
+  while (txt.length < len) txt = char + txt;
+  return txt;
+}
+
+function getSize(path, exclude, total) {
+  total = total ? total : 0;
+  fs.readdirSync(path).forEach(file => {
+    let subPath = path + '/' + file;
+    if(exclude.test(subPath)) {
+      return;
+    }
+    let stats = fs.lstatSync(subPath);
+    console.log(`${rightPad(subPath, 40)} ${leftPad(stats.size, 10)} bytes`);
+    total += stats.size;
+    if(stats.isDirectory()) {
+      total += getSize(subPath, exclude, total);
+    }
+  });
+  return total;
+}
+
+let total = getSize('./dist', /gwgc201819_overlay\.png/);
+let limit = 128000;
+let percentage = 100*(total/limit);
+console.log(rightPad("", 60, "="));
+console.log(`${rightPad("TOTAL PACKAGE SIZE", 40)} ${leftPad(total, 10)} bytes`);
+console.log(`${rightPad("PACKAGE SIZE LIMIT", 40)} ${leftPad(limit, 10)} bytes`);
+console.log(`${rightPad("PERCENTAGE", 40)} ${leftPad(percentage.toFixed(2), 10)} %`);
